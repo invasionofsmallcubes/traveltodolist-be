cd1e5e50949e87f4da70a0df4bb02c6004c264ee
Emanuele Ianni <dierre@gmail.com>
2018-12-15 10:53:53 +0100

    removed javascript

--- src/main/javascript/src/TaskList.ts
+++
@@ -1,9 +0,0 @@
-export default class TaskList {
-
-    public readonly tasks: any[];
-
-    constructor(tasks: any[]) {
-        this.tasks = tasks;
-    }
-
-}
--- src/main/javascript/src/setupTests.ts
+++
@@ -1,5 +0,0 @@
-import { configure } from 'enzyme';
-// @ts-ignore
-import * as Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
