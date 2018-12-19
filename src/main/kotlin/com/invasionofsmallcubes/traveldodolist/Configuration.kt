e8dd5ce5cb50ae5955964e0d2defd593b8b2489e
Emanuele Ianni <dierre@gmail.com>
2018-12-19 19:43:33 +0100

    added mongo connection

--- src/main/kotlin/com/invasionofsmallcubes/traveldodolist/Configuration.kt
+++ src/main/kotlin/com/invasionofsmallcubes/traveldodolist/Configuration.kt
@@ -1,15 +1,20 @@
 package com.invasionofsmallcubes.traveldodolist
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.core.MongoTemplate
 
 
 @Configuration
 class Configuration {
 
+    @Autowired
+    private lateinit var mongoTemplate: MongoTemplate
+
     @Bean
     fun tripRepository(): TripRepository {
-        return InMemoryTripRepository()
+        return MongoTripRepository(mongoTemplate)
     }
 
     @Bean
--- src/main/kotlin/com/invasionofsmallcubes/traveldodolist/MongoTripRepository.kt
+++ src/main/kotlin/com/invasionofsmallcubes/traveldodolist/MongoTripRepository.kt
@@ -1,10 +1,14 @@
 package com.invasionofsmallcubes.traveldodolist
 
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
 
 class MongoTripRepository(private val mongoTemplate: MongoTemplate) : TripRepository {
     override fun findByOwner(owner: String): List<Trip> {
-        return emptyList()
+        val query = Query()
+        query.addCriteria(Criteria.where("owner").`is`(owner))
+        return mongoTemplate.find(query, Trip::class.java)
     }
 
     override fun find(id: String): Trip? {
